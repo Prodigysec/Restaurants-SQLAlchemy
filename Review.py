6d70c6fd5f575202b43b8f334a74f1a686548c47
Ian Raburu <williamraburu@gmail.com>
2023-09-04 14:43:07 +0300

    Create Customer and Restaurant foreign key that links to their respective id

--- Review.py
+++ Review.py
@@ -6,6 +6,6 @@ class Review(Base):
     __tablename__ = "reviews"
 
     id = Column(Integer, primary_key=True, index=True)
-    restaurant_id = Column(Integer(),nullable=False)
-    Customer_id = Column(Integer(), nullable=False) 
+    restaurant_id = Column(Integer(), ForeignKey('restaurants.id'),nullable=False)
+    Customer_id = Column(Integer(), ForeignKey('customers.id'), nullable=False) 
     star_rating = Column(Integer(), nullable=False)
