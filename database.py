81108da70810151c4df9f804c0904360237618d3
Ian Raburu <williamraburu@gmail.com>
2023-09-04 14:23:27 +0300

    Create the database connection

---
+++ database.py
@@ -0,0 +1,13 @@
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///restaurants.db', connect_args={'check_same_thread': False})
+
+    # Create a session to connect to the database
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+Base.metadata.create_all(engine)
