87119bbd314b880d77e17174b151a133e9d1c963
Ian Raburu <williamraburu@gmail.com>
2023-09-04 14:58:53 +0300

    Connect to Review table

--- Customer.py
+++ Customer.py
@@ -1,4 +1,5 @@
 from .database import Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 
 
@@ -8,3 +9,5 @@ class Customer(Base):
     id = Column(Integer, primary_key=True, index=True)
     first_name = Column(String(), nullable=False)
     last_name = Column(String(), nullable=False)
+
+    reviews = relationship('Review', back_populates='customer')
