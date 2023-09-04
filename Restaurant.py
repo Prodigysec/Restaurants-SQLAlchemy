a908a648190229109d0b616e7f591afff6449fc5
Ian Raburu <williamraburu@gmail.com>
2023-09-04 14:59:12 +0300

    Connect to Review table

--- Restaurant.py
+++ Restaurant.py
@@ -1,4 +1,5 @@
 from .database import Base
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Integer, String
 
 
@@ -8,3 +9,5 @@ class Restaurant(Base):
     id = Column(Integer(), primary_key=True, index=True)
     name = Column(String(), nullable=False)
     price = Column(Integer(), nullable=False)
+
+    reviews = relationship('Review', back_populates='restaurant')
