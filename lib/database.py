3094b3331d9a48322a5773b96b307db51bc6a0d7
Ian Raburu <williamraburu@gmail.com>
2023-09-05 15:09:56 +0300

    Remove code execution restriction

--- lib/database.py
+++ lib/database.py
@@ -2,11 +2,11 @@
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 
-if __name__ == '__main__':
-    engine = create_engine('sqlite:///restaurants.db', connect_args={'check_same_thread': False})
 
-    # Create a session to connect to the database
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine('sqlite:///restaurants.db', connect_args={'check_same_thread': False})
+
+# Create a session to connect to the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
