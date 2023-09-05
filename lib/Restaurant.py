f9ce900186f4700af8b5c506e78ed62b868a65a0
Ian Raburu <williamraburu@gmail.com>
2023-09-05 23:24:56 +0300

    Create class properties

--- lib/Restaurant.py
+++ lib/Restaurant.py
@@ -11,3 +11,20 @@ class Restaurant(Base):
     price = Column(Integer(), nullable=False)
 
     reviews = relationship('Review', back_populates='restaurant')
+
+    @property
+    def __repr__(self):
+        return f'Restaurant(id={self.id}, ' + \
+            f'name={self.name}, ' + \
+            f'price={self.price})'
+
+    @classmethod
+    def fanciest_restaurant(session):
+        fanciest_restaurant = session.query(Restaurant).order_by(Restaurant.price.desc()).first()
+        return fanciest_restaurant
+    
+    
+    @property
+    def all_reviews(self):
+        return [f'Review for {self.name} by {review.customer.full_name}: {review.star_rating} stars.' for review in self.reviews]
+
